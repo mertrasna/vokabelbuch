75eacf3fd0104b4c8d1feb1be36f8b3a95d872fc
Mert Rasna <mrasnalu02@gmail.com>
2025-07-21 02:09:49 +0200

    new rules firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -23,3 +23,5 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export const auth = getAuth(app);
 export { db };
+
+// NOTE: Make sure to update Firestore security rules to allow quizResults collection access
